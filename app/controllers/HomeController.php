a85972bc2d3ea0183afd8f8931b7ef36a16a7e75
Joe Pereira <96992081+JoePereiraa@users.noreply.github.com>
2023-11-07 23:18:42 -0300

    V1

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\controllers;
+
+use app\controllers\Controller;
+
+class HomeController
+{
+
+    public function index($params)
+    {
+        var_dump($params->name);
+        return Controller::view('home');
+    }
+}
